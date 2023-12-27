f29a4f79c891865c8608917c1b72c603a517ddfa
帆 <1157712386@qq.com>
2023-12-27 17:45:10 +0800

    增加腐蚀算法，增加sdk注释

---
+++ MySTNode/Operator/Eroding.cs
@@ -0,0 +1,40 @@
+﻿using _305Vision.Utils;
+using _305Vision.图片操作测试;
+using ST.Library.UI.NodeEditor;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _305Vision.MySTNode.Operator
+{
+    [STNode("/算子","图像腐蚀")]
+    public class Eroding : ImageBaseNode
+    {
+
+        private STNodeOption in_option;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            this.Title = "图像腐蚀";
+
+            in_option = this.InputOptions.Add("输入图像", typeof(Image), true);
+            in_option.DataTransfer += In_option_DataTransfer;
+        }
+
+        private void In_option_DataTransfer(object sender, STNodeOptionEventArgs e)
+        {
+            
+        }
+
+        protected override void OnDrawBody(DrawingTools dt)
+        {
+            base.OnDrawBody(dt);
+            MyDrawBody myDrawBody = new MyDrawBody();
+            myDrawBody.DrawBody(dt, m_img_draw,this.Left,this.Right);
+        }
+    }
+}
--- SDK/OpencvSDK.cs
+++ SDK/OpencvSDK.cs
@@ -9,18 +9,64 @@ using System.Threading.Tasks;
 namespace _305Vision.SDK
 {
     class OpenCVSDK
-    {
+    {   /// <summary>
+        /// 灰度处理
+        /// </summary>
+        /// <param name="data">图像指针</param>
+        /// <param name="width">图像宽</param>
+        /// <param name="height">图像高</param>
+        /// <param name="stride">图像步长</param>
+        /// <returns>图像指针</returns>
         [DllImport("demo.dll", EntryPoint = "grayScale", CallingConvention = CallingConvention.Cdecl/*, CallingConvention = CallingConvention.Cdecl*/)]
         public static extern unsafe byte* grayScale(IntPtr data, int width, int height, int stride);
+        /// <summary>
+        /// 释放指针资源
+        /// </summary>
+        /// <param name="intPtr">指针</param>
+        /// <returns>IntPtr</returns>
         [DllImport("demo.dll", EntryPoint = "releaseBuffer", CallingConvention = CallingConvention.Cdecl/*, CallingConvention = CallingConvention.Cdecl*/)]
         public static extern unsafe byte* releaseBuffer(IntPtr intPtr);
-
+        /// <summary>
+        /// 二值化
+        /// </summary>
+        /// <param name="intPtr">图像指针</param>
+        /// <param name="width">图像宽</param>
+        /// <param name="height">图像高</param>
+        /// <param name="stride">图像步长</param>
+        /// <param name="thresh">阈值</param>
+        /// <param name="max">大于阈值设置值</param>
+        /// <param name="min">小于阈值设置值</param>
+        /// <returns>图像指针</returns>
         [DllImport("demo.dll", EntryPoint = "binaryzation", CallingConvention = CallingConvention.Cdecl/*, CallingConvention = CallingConvention.Cdecl*/)]
         public static extern unsafe byte* binaryzation(IntPtr intPtr, int width, int height, int stride, int thresh, int max, int min);
-
+        /// <summary>
+        /// 图像腐蚀
+        /// </summary>
+        /// <param name="intPtr"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="stride"></param>
+        /// <param name="kerSize"></param>
+        /// <param name="kerStr"></param>
+        /// <param name="pointX"></param>
+        /// <param name="pointY"></param>
+        /// <param name="iterations"></param>
+        /// <returns></returns>
         [DllImport("demo.dll", EntryPoint = "eroding", CallingConvention = CallingConvention.Cdecl/*, CallingConvention = CallingConvention.Cdecl*/)]
         public static extern unsafe byte* eroding(IntPtr intPtr, int width, int height, int stride, int kerSize, int kerStr, int pointX, int pointY, int iterations);
-
+        /// <summary>
+        /// 图像扩展
+        /// </summary>
+        /// <param name="intPtr"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="stride"></param>
+        /// <param name="kerSize"></param>
+        /// <param name="kerStr"></param>
+        /// <param name="pointX"></param>
+        /// <param name="pointY"></param>
+        /// <param name="iterations"></param>
+        /// <returns></returns>
         [DllImport("demo.dll", EntryPoint = "expansion", CallingConvention = CallingConvention.Cdecl/*, CallingConvention = CallingConvention.Cdecl*/)]
         public static extern unsafe byte* expansion(IntPtr intPtr, int width, int height, int stride, int kerSize, int kerStr, int pointX, int pointY, int iterations);
 
